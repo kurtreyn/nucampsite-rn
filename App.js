3f626183d146874cdafad995b5608ddf37bbf38c
kurtreyn <kurtreyn@yahoo.com>
2022-01-22 11:43:59 -0500

    added LogBox ignore for error messages to App.js

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { LogBox } from 'react-native';
 import Main from './components/MainComponent';
 
 export default function App() {
   return <Main />;
 }
+
+LogBox.ignoreAllLogs(true);
+// LogBox.ignoreLogs('');
